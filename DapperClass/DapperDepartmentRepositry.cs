14881ece02fd0368998b15e5426c6242d0561531
mraeb <mraeburn111@gmail.com>
2020-07-23 19:49:32 -0500

    setup dapper functionality

---
+++ DapperClass/DapperDepartmentRepositry.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace DapperClass
+{
+    public class DapperDepartmentRepositry : IDepartmentRepository
+    {
+
+        private readonly IDbConnection _connection;
+        public DapperDepartmentRepositry(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+           return _connection.Query<Department>("SELECT * FROM departments;");
+        }
+    }
+}
---
+++ DapperClass/IDepartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DapperClass
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAllDepartments();
+    }
+}
